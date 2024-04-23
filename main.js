46ae5a9015bfbf9533b47f007ae91f6950100009
Oswaldo Aparicio <oapariciomar@gmail.com>
2024-04-22 22:59:22 -0500

    curso_2_alura

---
+++ main.js
@@ -0,0 +1,23 @@
+function playSonidoPom(){
+    document.querySelector('#sonido_tecla_pom').play();
+}
+
+
+document.querySelectorAll('.tecla')
+
+//document.querySelector('.tecla_pom').onclick = playSonidoPom;
+
+
+
+
+
+
+
+
+/*
+function playSonidoClap(){
+    document.querySelector('#sonido_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = playSonidoClap;
+*/
